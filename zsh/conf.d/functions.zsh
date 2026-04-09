60508bc0f5250d49cf2290e3d2374580c400d827
Anatoli Nicolae <an@thundersquared.com>
2026-04-09 22:51:47 +0200

    feat: add aliases and functions
    Signed-off-by: Anatoli Nicolae <an@thundersquared.com>

--- zsh/conf.d/functions.zsh
+++ zsh/conf.d/functions.zsh
@@ -1,1 +1,350 @@
-# Shell functions — add your own below
+# Shell functions
+
+# Simple calculator
+function calc() {
+  local result="";
+  result="$(printf "scale=10;$*\n" | bc --mathlib | tr -d '\\\n')";
+  #                       └─ default (when `--mathlib` is used) is 20
+  #
+  if [[ "$result" == *.* ]]; then
+    # improve the output for decimal numbers
+    printf "$result" |
+    sed -e 's/^\./0./'        `# add "0" for cases like ".5"` \
+        -e 's/^-\./-0./'      `# add "0" for cases like "-.5"`\
+        -e 's/0*$//;s/\.$//';  # remove trailing zeros
+  else
+    printf "$result";
+  fi;
+  printf "\n";
+}
+
+# Create a new directory and enter it
+function mkd() {
+  mkdir -p "$@" && cd "$_";
+}
+
+# Change working directory to the top-most Finder window location
+function cdf() { # short for `cdfinder`
+  cd "$(osascript -e 'tell app "Finder" to POSIX path of (insertion location as alias)')";
+}
+
+# Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
+function targz() {
+  local tmpFile="${@%/}.tar";
+  tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1;
+
+  size=$(
+    stat -f"%z" "${tmpFile}" 2> /dev/null; # OS X `stat`
+    stat -c"%s" "${tmpFile}" 2> /dev/null # GNU `stat`
+  );
+
+  local cmd="";
+  if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
+    # the .tar file is smaller than 50 MB and Zopfli is available; use it
+    cmd="zopfli";
+  else
+    if hash pigz 2> /dev/null; then
+      cmd="pigz";
+    else
+      cmd="gzip";
+    fi;
+  fi;
+
+  echo "Compressing .tar using \`${cmd}\`…";
+  "${cmd}" -v "${tmpFile}" || return 1;
+  [ -f "${tmpFile}" ] && rm "${tmpFile}";
+  echo "${tmpFile}.gz created successfully.";
+}
+
+# Determine size of a file or total size of a directory
+function fs() {
+  if du -b /dev/null > /dev/null 2>&1; then
+    local arg=-sbh;
+  else
+    local arg=-sh;
+  fi
+  if [[ -n "$@" ]]; then
+    du $arg -- "$@";
+  else
+    du $arg .[^.]* *;
+  fi;
+}
+
+# Use Git’s colored diff when available
+hash git &>/dev/null;
+if [ $? -eq 0 ]; then
+  function gitdiff() {
+    git diff --no-index --color-words "$@";
+  }
+fi;
+
+# Create a data URL from a file
+function dataurl() {
+  local mimeType=$(file -b --mime-type "$1");
+  if [[ $mimeType == text/* ]]; then
+    mimeType="${mimeType};charset=utf-8";
+  fi
+  echo "data:${mimeType};base64,$(openssl base64 -in "$1" | tr -d '\n')";
+}
+
+# Create a git.io short URL
+function gitio() {
+  if [ -z "${1}" -o -z "${2}" ]; then
+    echo "Usage: \`gitio slug url\`";
+    return 1;
+  fi;
+  curl -i http://git.io/ -F "url=${2}" -F "code=${1}";
+}
+
+# Start an HTTP server from a directory, optionally specifying the port
+function server() {
+  local port="${1:-8000}";
+  sleep 1 && open "http://localhost:${port}/" &
+  # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+  # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+  python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port";
+}
+
+# Start a PHP server from a directory, optionally specifying the port
+# (Requires PHP 5.4.0+.)
+function phpserver() {
+  local port="${1:-4000}";
+  local ip=$(ipconfig getifaddr en1);
+  sleep 1 && open "http://${ip}:${port}/" &
+  php -S "${ip}:${port}";
+}
+
+# Compare original and gzipped file size
+function gz() {
+  local origsize=$(wc -c < "$1");
+  local gzipsize=$(gzip -c "$1" | wc -c);
+  local ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l);
+  printf "orig: %d bytes\n" "$origsize";
+  printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio";
+}
+
+# Run `dig` and display the most useful info
+function digga() {
+  dig +nocmd "$1" any +multiline +noall +answer;
+}
+
+# UTF-8-encode a string of Unicode symbols
+function escape() {
+  printf "\\\x%s" $(printf "$@" | xxd -p -c1 -u);
+  # print a newline unless we’re piping the output to another program
+  if [ -t 1 ]; then
+    echo ""; # newline
+  fi;
+}
+
+# Decode \x{ABCD}-style Unicode escape sequences
+function unidecode() {
+  perl -e "binmode(STDOUT, ':utf8'); print \"$@\"";
+  # print a newline unless we’re piping the output to another program
+  if [ -t 1 ]; then
+    echo ""; # newline
+  fi;
+}
+
+# Get a character’s Unicode code point
+function codepoint() {
+  perl -e "use utf8; print sprintf('U+%04X', ord(\"$@\"))";
+  # print a newline unless we’re piping the output to another program
+  if [ -t 1 ]; then
+    echo ""; # newline
+  fi;
+}
+
+# Show all the names (CNs and SANs) listed in the SSL certificate
+# for a given domain
+function getcertnames() {
+  if [ -z "${1}" ]; then
+    echo "ERROR: No domain specified.";
+    return 1;
+  fi;
+
+  local domain="${1}";
+  echo "Testing ${domain}…";
+  echo ""; # newline
+
+  local tmp=$(echo -e "GET / HTTP/1.0\nEOT" \
+    | openssl s_client -connect "${domain}:443" -servername "${domain}" 2>&1);
+
+  if [[ "${tmp}" = *"-----BEGIN CERTIFICATE-----"* ]]; then
+    local certText=$(echo "${tmp}" \
+      | openssl x509 -text -certopt "no_aux, no_header, no_issuer, no_pubkey, \
+      no_serial, no_sigdump, no_signame, no_validity, no_version");
+    echo "Common Name:";
+    echo ""; # newline
+    echo "${certText}" | grep "Subject:" | sed -e "s/^.*CN=//" | sed -e "s/\/emailAddress=.*//";
+    echo ""; # newline
+    echo "Subject Alternative Name(s):";
+    echo ""; # newline
+    echo "${certText}" | grep -A 1 "Subject Alternative Name:" \
+      | sed -e "2s/DNS://g" -e "s/ //g" | tr "," "\n" | tail -n +2;
+    return 0;
+  else
+    echo "ERROR: Certificate not found.";
+    return 1;
+  fi;
+}
+
+# `s` with no arguments opens the current directory in Sublime Text, otherwise
+# opens the given location
+function s() {
+  if [ $# -eq 0 ]; then
+    /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl .;
+  else
+    /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl "$@";
+  fi;
+}
+
+# `v` with no arguments opens the current directory in VS Code, otherwise opens the
+# given location
+function v() {
+  if [ $# -eq 0 ]; then
+    /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code .;
+  else
+    /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code "$@";
+  fi;
+}
+
+# `o` with no arguments opens the current directory, otherwise opens the given
+# location
+function o() {
+  if [ $# -eq 0 ]; then
+    open .;
+  else
+    open "$@";
+  fi;
+}
+
+# `tre` is a shorthand for `tree` with hidden files and color enabled, ignoring
+# the `.git` directory, listing directories first. The output gets piped into
+# `less` with options to preserve color and line numbers, unless the output is
+# small enough for one screen.
+function tre() {
+  tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
+}
+
+function set_proxy() {
+  export http_proxy=´http://$1:$2´
+  export https_proxy=´https://$1:$2´
+}
+
+# Search all directory files for a string
+function searchfiles() {
+  grep -rnw $1 -e $2
+}
+
+# Auto-retry command
+function auto-retry()
+{
+    false
+    while [ $? -ne 0 ]; do
+        "$@" || (sleep 1;false)
+    done
+}
+
+function githasha()
+{
+    sha1sum $1 | cut -c1-7
+}
+
+function gdrive_download () {
+  CONFIRM=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$1" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
+  wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM&id=$1" -O $2
+  rm -rf /tmp/cookies.txt
+}
+
+function curl_cache () {
+  curl -svo /dev/null $1
+}
+
+function ifupdown () {
+  sudo ifconfig $1 down
+  sudo ifconfig $1 up
+}
+
+function mp3tomp4cover () {
+  MP3=$1
+  FILE=${MP3/mp3/mp4}
+
+  ffmpeg -f lavfi -i smptebars=size=1280x720:rate=23.976 -i $MP3 \
+  -c:v libx264 -profile:v main -c:a aac \
+  -shortest -movflags +faststart -preset ultrafast -loglevel error $FILE
+
+  open -R $FILE
+}
+
+function aria-dl () {
+  aria2c -x 16 --file-allocation=none $@
+}
+
+function share-file-io () {
+  if [ -f "$1" ]; then
+    curl -F "file=@$1" 'https://file.io/?expires=100y'
+  else
+    curl --data "text=$1" 'https://file.io/?expires=100y'
+  fi
+}
+
+function clean-provisioning-profiles () {
+  rm ~/Library/MobileDevice/Provisioning\ Profiles/*.mobileprovision
+  open /Applications/Xcode.app
+}
+
+function aria-retry () {
+    URL=$1
+    filename=$(basename $URL)
+    maxsize=1000000
+    retrydl=0
+
+    cd /Users/anatoli/Downloads
+    touch "$filename"
+
+    while [ "$retrydl" -eq 0 ]; do
+
+        filesize=$(stat -c%s "$filename")
+        echo "Size of $filename = $filesize bytes."
+
+        if (( filesize < maxsize )); then
+            aria-dl -o "$filename" "$URL"
+        else
+            retrydl=1
+        fi
+
+    done
+
+
+    echo "Downloaded $filename!"
+}
+
+function zstdc() {
+    SOURCE="$1"
+    OUTPUT=$(basename "$SOURCE" | cut -d. -f1)
+    gtar -I "zstd -10 -T0 -M2048" -cf "$OUTPUT.tar.zst" "$SOURCE"
+}
+
+function zstdx() {
+    SOURCE="$1"
+    gtar -xaf "$SOURCE"
+}
+
+function ffmpegjoin() {
+  ROOTDIR=$(pwd | basename)
+
+  PARTS=("front" "back" "left_repeater" "right_repeater")
+
+  for PART in "${PARTS[@]}"
+  do
+    [ -e "$PART.txt" ] && rm "$PART.txt"
+
+    for f in *-"$PART".mp4
+    do
+      echo "file $f" >> "$PART.txt"
+    done
+
+    ffmpeg -f concat -i "$PART.txt" -c copy "$ROOTDIR-$PART.mp4" && rm "$PART.txt"
+  done
+}
