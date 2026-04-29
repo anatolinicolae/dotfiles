84a14e9e556f2545e4459e3104dba9413172059e
Anatoli Nicolae <an@thundersquared.com>
2026-04-29 23:13:18 +0200

    fix: brewup install everything not just casks
    Signed-off-by: Anatoli Nicolae <an@thundersquared.com>

--- zsh/conf.d/aliases.zsh
+++ zsh/conf.d/aliases.zsh
@@ -1,6 +1,6 @@
 # Update Homebrew packages for this machine's profile.
 # $DOTFILES and $DOTFILES_PROFILE are set in .zshenv before .zshrc runs.
-alias brewup='brew bundle --file="$DOTFILES/profiles/$DOTFILES_PROFILE/Brewfile" --upgrade'
+alias brewup='brew bundle install --file="$DOTFILES/profiles/$DOTFILES_PROFILE/Brewfile" --upgrade --cleanup --brews --casks --mas --jobs=auto'
 
 # Easier navigation: .., ..., ...., ....., ~ and -
 alias ..="cd .."
